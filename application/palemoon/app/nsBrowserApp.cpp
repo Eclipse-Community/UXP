30a9216d11beabfec24216f2ea2cabd9f9104984
K4sum1 <k4sum1@eclipse.cx>
2025-04-27 18:09:04 -0700

    Branding inprogress

--- application/palemoon/app/nsBrowserApp.cpp
+++ application/palemoon/app/nsBrowserApp.cpp
@@ -75,7 +75,7 @@ static void Output(const char *fmt, ... )
     decltype(MessageBoxW)* messageBoxW =
       (decltype(MessageBoxW)*) GetProcAddress(user32, "MessageBoxW");
     if (messageBoxW) {
-      messageBoxW(nullptr, wide_msg, L"New Moon", MB_OK
+      messageBoxW(nullptr, wide_msg, L"Lun3r", MB_OK
                                                | MB_ICONERROR
                                                | MB_SETFOREGROUND);
     }
